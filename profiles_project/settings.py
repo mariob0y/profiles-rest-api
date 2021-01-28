bd3f8ddd2324a11f96202fd085df7aa838cfea0e
mariob0y <suroviymarioboy@gmail.com>
2021-01-28 12:36:29 +0200

    Added custom user profile model and manager for it

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Changing user model from default to custom
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
