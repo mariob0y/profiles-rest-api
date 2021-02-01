341acd483d2a0581e2891d88969fd31d2ce74668
mariob0y <suroviymarioboy@gmail.com>
2021-02-01 12:36:25 +0200

    Added profiles viewset, permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,41 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
-    '''Serializes a namefield for testing APIView'''
+    '''Serializes a name field for testing APIView'''
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    '''Serializes a user profile object'''
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        # To make sure that password won't me retrieved, to use it only
+        # when create new object
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        '''Create and return new user
+        (overrriding defaault create function)'''
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
