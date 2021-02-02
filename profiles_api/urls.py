45a506632aa9efd61212653d94b47d5b0b0841e9
mariob0y <suroviymarioboy@gmail.com>
2021-02-02 10:49:48 +0200

    Added feed and authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,7 +7,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
-
+router.register('feed', views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
